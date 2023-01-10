c4a6ecbe7f8168a0fee6c5fb0278020bdf6d888c
Maryan121 <88990104+Maryan121@users.noreply.github.com>
2023-01-10 21:44:23 +0300

    Update main.js

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ let myForm = document.getElementById('form-field');
     let errorMsg = document.getElementById('errormgs');
     btn.addEventListener('click',ValidateEmail);
 
-    function ValidateEmail(e) 
+    function ValidateEmail() 
     {
-        e.preventDeafult();
+       
       if(input.value == ''){
         input.style.border = 'hsl(354, 100%, 66%) solid 1px';
         errorMsg.style.display = 'block';
